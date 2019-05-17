fcf8cfccd4187e5117b0cd1511262e62b4f7e61e
Rich Lysogorski <rmlysogorski@gmail.com>
2019-05-16 22:15:25 -0400

    First commit

---
+++ PrimeNumberGenerator/PrimeNumberGenerator.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PrimeNumberGenerator.aspx.cs" Inherits="PrimeNumberGenerator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="display:inline-block;padding:10px 10px 10px 10px">
+            <p>Let's generate some prime numbers!</p>
+            Enter a number:&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged" TextMode="Number"></asp:TextBox>
+            <asp:Button ID="PrimeButton" runat="server" Text="Generate Prime Number!" OnClick="PrimeButton_Click" />
+            <br /><br />
+            <asp:Label ID="PrimeNumberLabel" runat="server" Text="Label" Visible="False"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PrimeNumberGenerator/PrimeNumberGenerator.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PrimeNumberGenerator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PrimeButton_Click(object sender, EventArgs e)
+        {
+            int textboxInput;
+
+            if (TextBox1.Text == string.Empty)
+            {
+                PrimeNumberLabel.Text = "Please enter a number!";
+            }
+            else if (int.Parse(TextBox1.Text) <= 0)
+            {
+                PrimeNumberLabel.Text = "Please enter a positive number greater than 0!";
+            }
+            else
+            {
+                textboxInput = int.Parse(TextBox1.Text);
+                int prime = 2;
+                int count = 1;
+                bool isPrime;
+                for (int i = 3; count < textboxInput; i += 2)
+                {
+                    isPrime = true;
+                    for (int j = 3; j <= Math.Sqrt(i); j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        prime = i;
+                        count++;
+                    }
+                }
+                PrimeNumberLabel.Text = $"The number {textboxInput.ToString()} prime number is {prime}.";
+            }
+
+            PrimeNumberLabel.Visible = true;
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
